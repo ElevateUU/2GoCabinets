5a710633b5250c67e47dfec59af7dd56312bbec8
ElevateU <elevateu@ElevateUs-MacBook-Pro.local>
2023-05-15 17:36:45 -0400

    fix: another err

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "@/styles/Steps.module.css"
 
 function App({ Component, pageProps }: AppProps) {
   return (
